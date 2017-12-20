28d07fa3554235e92d615a794679968f9ffa0954
Blaise Gervais <blaise.gervais@fednot.be>
2017-12-20 15:34:43 +0100

    Initialization

---
+++ src/main/java/q183272/Q183272.java
@@ -0,0 +1,26 @@
+package q183272;
+
+/*
+ * I had an interview yesterday where I was asked to write a function that would  
+ * in a list containing a date and the amount of money spent on that date, and to  
+ * the total amount spent per month. The data was for an energy company, so the  
+ * They left it kind of open after that on how to proceed. I didn't manage to get  
+ * Month-Year   |  Usage 
+--------------------------------
+January-2017     |   
+ *    |  1250.0
+April-2017       |  100.45
+May-2017         |  225.0
+December-2017  
+ * My code is below. Please let me know if you think there is simpler way to do  
+ * I thought maybe instead of a Map, I could have another class called  
+ * and make a list of that instead to hold the data, but I wasn't too sure how to  
+ */
+public class Q183272 {
+   public static void main(String[] args) {
+       System.out.println(
+           /* TODO, describe usage */);
+       
+       
+   }
+}
