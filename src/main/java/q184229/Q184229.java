0228ef54eb1b8c43c2225bcab045fd27bed59b4a
Blaise Gervais <blaise.gervais@fednot.be>
2018-01-04 13:21:33 +0100

    Initialization

---
+++ src/main/java/q184229/Q184229.java
@@ -0,0 +1,29 @@
+package q184229;
+
+/*
+ * A couple of weeks I had a coding interview and the problem to solve was very  
+ * public static String reverse(String phrase) {
+        String[] words =  
+ * ");
+        StringBuffer sb = new StringBuffer();
+        for (String word :  
+ * {
+            String reversed = new String();
+            for (int j =  
+ * - 1; j >= 0; j--) {
+                reversed += word.charAt(j);
+            }
+   
+ *          sb = sb.append(reversed).append(" ");
+        }
+
+        return  
+ */
+public class Q184229 {
+   public static void main(String[] args) {
+       System.out.println(
+           /* TODO, describe usage */);
+       
+       
+   }
+}
